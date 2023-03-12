367e58619c50d05c7494d6ff6db2b0202d0f9bfa
wholemann <wholeman.dev@gmail.com>
2023-03-13 01:50:51 +0900

    Micro Store 만들어보기 과제 풀이

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # frontend-survival-week06-answer
+
 프론트엔드 생존코스 6주차 과제 풀이
---
+++ src/hooks/useCounterStore.tsx
@@ -0,0 +1,17 @@
+import { useSyncExternalStore } from 'react';
+
+import CounterStore, { CounterStoreSnapshot } from '../stores/CounterStore';
+
+const counterStore = new CounterStore();
+
+export default function useCounterStore(): [CounterStoreSnapshot, CounterStore] {
+  const snapshot = useSyncExternalStore(
+    (onStoreChange) => {
+      counterStore.addListener(onStoreChange);
+      return () => counterStore.removeListener(onStoreChange);
+    },
+    () => counterStore.getSnapshot(),
+  );
+
+  return [snapshot, counterStore];
+}
